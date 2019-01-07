7c382c791fa0f3b520387467373bb00f770f6534
Heloise Taillet <ht2394@columbia.edu>
2019-01-07 07:01:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,8 +11,10 @@ def load_library
   emo_hash["get_meaning"] = {}
 
   emo.each do |english, emo|
-    binding.pry
+    emo_hash["get_emoticon"][emo.first] = emo.last 
+    emo_hash["get_meaning"][emo.last] = english
   end
+  emo_hash
 end
 
 def get_japanese_emoticon
