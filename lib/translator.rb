047325374e847e4415322dafbe6fd4fce04f97e7
Heloise Taillet <ht2394@columbia.edu>
2019-01-07 06:59:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,6 +2,15 @@
 require 'YAML'
 def load_library
   # code goes here
+  emo = YAML.load_file(path)
+  emo_hash = {}
+
+  emo_hash["get_emoticon"] = {}
+  emo_hash["get_meaning"] = {}
+
+  emo.each do |english, emo|
+    binding.pry
+  end
 end
 
 def get_japanese_emoticon
