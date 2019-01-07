6cf2206fcc5ba9b141044e358c29dd022baf19ff
Heloise Taillet <ht2394@columbia.edu>
2019-01-07 07:03:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require 'pry'
 require 'yaml'
 
-def load_library
+def load_library(path)
   # code goes here
   emo = YAML.load_file(path)
   emo_hash = {}
@@ -11,16 +11,22 @@ def load_library
   emo_hash["get_meaning"] = {}
 
   emo.each do |english, emo|
-    emo_hash["get_emoticon"][emo.first] = emo.last 
+    emo_hash["get_emoticon"][emo.first] = emo.last
     emo_hash["get_meaning"][emo.last] = english
   end
   emo_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emo)
   # code goes here
+    emo_hash = load_library(path)
+    find = emo_hash["get_emoticon"][emo]
+    if find == nil
+      find = "Sorry, that emoticon was not found" 
+    end
+  find
 end
 
-def get_english_meaning
+def get_english_meaning(path, emo)
   # code goes here
 end
